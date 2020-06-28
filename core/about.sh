a00328dc78a302cb05bcad44f6a5040b6f735029
Evil Devil <66058079+evildevill@users.noreply.github.com>
2020-06-28 07:16:00 +0500

    Update about.sh

--- core/about.sh
+++ core/about.sh
@@ -1,1 +1,24 @@
+clear
+echo '                    
+            __   __     _____   __       __       _____    
+           /\_\ /_/\  /\_____\ /\_\     /\_\     ) ___ (   
+          ( ( (_) ) )( (_____/( ( (    ( ( (    / /\_/\ \  
+           \ \___/ /  \ \__\   \ \_\    \ \_\  / /_/ (_\ \ 
+           / / _ \ \  / /__/_  / / /__  / / /__\ \ )_/ / / 
+          ( (_( )_) )( (_____\( (_____(( (_____(\ \/_\/ /  
+           \/_/ \_\/  \/_____/ \/_____/ \/_____/ )_____(   
+                                                 
+' | lolcat
+echo " "
+echo "                             About"|lolcat
+echo " "
+echo "       🙏 Hey, there I am MUHAMMAD WASIM AKRAM (W A S I), i made this tool
+  to penetrate password strength In termux, so i hope guys you
+                             liked it. 😘"
+echo ""
+echo "                  Our channel :- Hacker wasi"| lolcat
+echo " "
 
+sleep 5.0
+cd $HOME/instahack
+bash instahack.sh
