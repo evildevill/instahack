1198fdd893e1ea47b28da38bb9ac80f11defc168
Rishav Das <61372472+rdofficial@users.noreply.github.com>
2021-05-10 14:36:04 +0530

    Updating the instahack.sh script and removing some more errors inside it + also adding the infinite runtime for the tool
    In this commit, we made the following changes :
    1. Added the infinite loop feature of the main menu.
    2. Corrected some previous file errors.
    3. Changed the option number for 'exit' from 8 to 0 in the main menu of the tool. Reason is that, when more and newer options will be added to the tool, we have to jump these exit option too. That's why keeping it 0 will be constant forever. Just add new options after 7th option number.

--- instahack.sh
+++ instahack.sh
@@ -9,10 +9,9 @@
 # Last modified on : May 10, 2021
 #
 # Changes made in the last modification :
-# 1. Added commented docs + made the code structure more clean as well as now it is good for coders to read the actual code base
-# 2. Removed some older errors and updated the older version of the code to the newer and way that uses less number of lines of the code + added comments to each part of the script such that the programmers and hackers will understand the code even if they don't touch the source files for a while
-# 3. Adding more colors and making the proper use of the ANSII color code variables + using more bright colors for better console look of the tool.
-# 4. Removing the lolcat utility dependency, instead we are using plain colors to print it on the console screen. This makes the changes of error much more often. Like why installing another software for just printing a banner, as well as the software also requires ruby, ruby-environment and ruby-interpreter to be installed on the computer system. Thus, too many dependencies are there for this just one banner printing. So, we removed it.
+# 1. Added the infinite loop feature of the main menu.
+# 2. Corrected some previous file errors.
+# 3. Changed the option number for 'exit' from 8 to 0 in the main menu of the tool. Reason is that, when more and newer options will be added to the tool, we have to jump these exit option too. That's why keeping it 0 will be constant forever. Just add new options after 7th option number.
 #
 # Authors contributed to this project (Add your name below if you have contributed) :
 # 1. Wasim Akram (github:https://github.com/evildevill/, email:hackerwasi1@gmail.com)
@@ -33,24 +32,26 @@ DEFCOL="\033[00m"
 
 function checkUpdates() {
 	# The function to check for the updates of the tool, if available any. We will just fetch the version update notice file at the github mirror of this repository (project)
+
 	if wget --spider https://raw.githubusercontent.com/evildevill/instahack/master/update.v1.3 2>/dev/null; then
 		# If there exists any updates to this tool, then we mark for the update in the core/update.txt file
 
 		# echo "ig" > $HOME/instahack/core/update.txt
 		# ----
-		# We donot do that shit here, because we dont want to create a waste file for just marking purpose. Lets directly call the update.sh script
+		# We donot do that shit here, because we dont want to create a waste file for just marking purpose. Lets directly call the update.sh script if there are really any updates to the script
 		# ----
 		if [[ -f "bash $HOME/instahack/core/update.sh" ]]; then
 			# If the update.sh script exists in the core/ folder of this project, then we continue
 		
 			bash $HOME/instahack/core/update.sh
 		else
-			# If the update.sh script does not exists in the core/ of the folder of this project, then we display the error message on the console screen
+			# If the update.sh script does not exists, then we display the error message on the console screen
 
 			echo -e "\n${RED_REV}[ Error : core/update.sh is missing, please see the instructions on the README.md file and resolve the error ]${DEFCOL}"
-			echo -e "[${RED}!{DEFCOL}] Continuing without updating the tool..."
-			sleep 2.5
+			echo -e "[${RED}!${DEFCOL}] Continuing without updating the tool..."
+			sleep 2
 			clear
+		fi
 	fi
 
 	# Now, checking for updates in the script file
@@ -64,12 +65,12 @@ function checkUpdates() {
 		else
 			# If the core/patch.sh script file does not exists, then we display the error message on the console screen
 
-
 			echo -e "\n${RED_REV}[ Error : core/patch.sh is missing, please see the instructions on the README.md file and resolve the error ]${DEFCOL}"
-			echo -e "[${RED}!{DEFCOL}] Continuing without updating the tool..."
-			sleep 2.5
+			echo -e "[${RED}!${DEFCOL}] Continuing without updating the tool..."
+			sleep 2
 			clear
 		fi
+	fi
 }
 
 # Calling the checkUpdates function to check for any updates to this tool
@@ -77,11 +78,12 @@ checkUpdates
 
 # The main part of the script starts here
 clear
-echo
 
 # Checking if the TOR service is running or not
-if $( pgrep -x tor >/dev/null ); then
+if pgrep -x "tor" >/dev/null; then
 	# If the TOR service is running, then we continue
+
+	printf ""
 else
 	# If the TOR service is not running, then we display the error message on the console screen
 
@@ -89,109 +91,118 @@ else
 
 	# Displaying the steps for launching the TOR service to the user
 	echo -e "\nUse the below steps :"
-	echo -e "[$YELLOW}1${DEFCOL}] Open a terminal and launch the command '${GREEN}tor${DEFCOL}'."
-	echo -e "[$YELLOW}2${DEFCOL}] Then, launch this tool (script) again."
+	echo -e "[${YELLOW}1${DEFCOL}] Open a terminal and launch the command '${GREEN}tor${DEFCOL}'."
+	echo -e "[${YELLOW}2${DEFCOL}] Then, launch this tool (script) again."
+	sleep 1
 	exit 1  # Exiting the script
 fi
 
-# Displaying the banner on the console screen
-echo -e "${YELLOW}
-╭━━╮╱╱╱╱╱╭╮╱╱╱╭╮╱╱╱╱╱╱╱╭╮
-╰┫┣╯╱╱╱╱╭╯╰╮╱╱┃┃╱╱╱╱╱╱╱┃┃
-╱┃┃╭━╮╭━┻╮╭╋━━┫╰━┳━━┳━━┫┃╭╮
-╱┃┃┃╭╮┫━━┫┃┃╭╮┃╭╮┃╭╮┃╭━┫╰╯╯
-╭┫┣┫┃┃┣━━┃╰┫╭╮┃┃┃┃╭╮┃╰━┫╭╮╮
-╰━━┻╯╰┻━━┻━┻╯╰┻╯╰┻╯╰┻━━┻╯╰╯
-${DEFCOL}"
-echo -e "\n                \e[101m\e[1;77m  >>  Script By Hacker wasi << \e[0m\n"
-echo -e "\n       \e[100m\e[1;77m >>  Youtube Channel : \e[1;96m Hacker wasi \e[100m\e[1;77m << \e[0m\n\n"
 
-# Displaying the main menu options and asking the user to enter a choice
-echo -e "\e[1;92m[\e[0m\e[1;77m1\e[0m\e[1;92m]\e[0m\e[1;93m Auto Attack\e[0m"
-echo -e "\e[1;92m[\e[0m\e[1;77m2\e[0m\e[1;92m]\e[0m\e[1;93m Manual Attack\e[0m"
-echo -e "\e[1;92m[\e[0m\e[1;77m3\e[0m\e[1;92m]\e[0m\e[1;93m About\e[0m"
-echo -e "\e[1;92m[\e[0m\e[1;77m4\e[0m\e[1;92m]\e[0m\e[1;93m Update\e[0m"
-echo -e "\e[1;92m[\e[0m\e[1;77m5\e[0m\e[1;92m]\e[0m\e[1;93m Subscribe\e[0m"
-echo -e "\e[1;92m[\e[0m\e[1;77m6\e[0m\e[1;92m]\e[0m\e[1;93m Chat now\e[0m"
-echo -e "\e[1;92m[\e[0m\e[1;77m7\e[0m\e[1;92m]\e[0m\e[1;93m Follow\e[0m"
-echo -e "\e[1;92m[\e[0m\e[1;77m8\e[0m\e[1;92m]\e[0m\e[1;93m Exit\e[0m\n"
-read -p $'\n\e[1;92m[\e[0m\e[1;77m*\e[0m\e[1;92m] Choose an option: \e[0m' option
+while true; do
+	# Using a while true loop for infinite execution of the script until the user chooses to exit
 
-# Checking the user entered option
-if [[ $option == 1 || $option == 01 ]]; then
-	# If the user choosed the option for auto attack
+	# Displaying the banner on the console screen
+	clear
+	echo -e "${YELLOW}
+	╭━━╮╱╱╱╱╱╭╮╱╱╱╭╮╱╱╱╱╱╱╱╭╮
+	╰┫┣╯╱╱╱╱╭╯╰╮╱╱┃┃╱╱╱╱╱╱╱┃┃
+	╱┃┃╭━╮╭━┻╮╭╋━━┫╰━┳━━┳━━┫┃╭╮
+	╱┃┃┃╭╮┫━━┫┃┃╭╮┃╭╮┃╭╮┃╭━┫╰╯╯
+	╭┫┣┫┃┃┣━━┃╰┫╭╮┃┃┃┃╭╮┃╰━┫╭╮╮
+	╰━━┻╯╰┻━━┻━┻╯╰┻╯╰┻╯╰┻━━┻╯╰╯
+	${DEFCOL}"
+	echo -e "\n                \e[101m\e[1;77m  >>  Script By Hacker wasi << \e[0m\n"
+	echo -e "\n       \e[100m\e[1;77m >>  Youtube Channel : \e[1;96m Hacker wasi \e[100m\e[1;77m << \e[0m\n\n"
 
-	if [[ -f "$HOME/instahack/main/auto.sh" ]]; then
-		# If the main/auto.sh script file exists, then we continue
+	# Displaying the main menu options and asking the user to enter a choice
+	echo -e "\e[1;92m[\e[0m\e[1;77m1\e[0m\e[1;92m]\e[0m\e[1;93m Auto Attack\e[0m"
+	echo -e "\e[1;92m[\e[0m\e[1;77m2\e[0m\e[1;92m]\e[0m\e[1;93m Manual Attack\e[0m"
+	echo -e "\e[1;92m[\e[0m\e[1;77m3\e[0m\e[1;92m]\e[0m\e[1;93m About\e[0m"
+	echo -e "\e[1;92m[\e[0m\e[1;77m4\e[0m\e[1;92m]\e[0m\e[1;93m Update\e[0m"
+	echo -e "\e[1;92m[\e[0m\e[1;77m5\e[0m\e[1;92m]\e[0m\e[1;93m Subscribe\e[0m"
+	echo -e "\e[1;92m[\e[0m\e[1;77m6\e[0m\e[1;92m]\e[0m\e[1;93m Chat now\e[0m"
+	echo -e "\e[1;92m[\e[0m\e[1;77m7\e[0m\e[1;92m]\e[0m\e[1;93m Follow\e[0m"
+	echo -e "\e[1;92m[\e[0m\e[1;77m0\e[0m\e[1;92m]\e[0m\e[1;93m Exit\e[0m\n"
+	read -p $'\n\e[1;92m[\e[0m\e[1;77m*\e[0m\e[1;92m] Choose an option: \e[0m' option
 
-		bash $HOME/instahack/main/auto.sh
-	else
-		# If the main/auto.sh script file does not exists, then we display the error on the console screen
+	# Checking the user entered option
+	if [[ $option == 1 || $option == 01 ]]; then
+		# If the user choosed the option for auto attack
 
-		echo -e "\n${RED_REV}[ Error : main/auto.sh file missing. Fix this error by getting the proper script file ]${DEFCOL}"
+		if [[ -f "$HOME/instahack/main/auto.sh" ]]; then
+			# If the main/auto.sh script file exists, then we continue
+
+			bash $HOME/instahack/main/auto.sh
+		else
+			# If the main/auto.sh script file does not exists, then we display the error on the console screen
+
+			echo -e "\n${RED_REV}[ Error : main/auto.sh file missing. Fix this error by getting the proper script file ]${DEFCOL}"
+			exit 1
+		fi
+	elif [[ $option == 2 || $option == 02 ]]; then
+		# If the user choosed the option for manual attack
+
+		if [[ -f "$HOME/instahack/main/manual.sh" ]]; then
+			# If the main/manual.sh script file exists, then we continue
+
+			bash $HOME/instahack/main/manual.sh
+		else
+			# If the main/manual.sh script file does not exists, then we display the error on the console screen
+
+			echo -e "\n${RED_REV}[ Error : main/manual.sh file missing. Fix this error by getting the proper script file ]${DEFCOL}"
+			exit 1
+		fi
+	elif [[ $option == 3 || $option == 03 ]]; then
+		# If the user choosed the option for about info displaying
+
+		if [[ -f "$HOME/instahack/core/about.sh" ]]; then
+			# If the core/about.sh script file exists, then we continue
+
+			bash $HOME/instahack/core/about.sh
+		else
+			# If the core/about.sh script file does not exists, then we display the error on the console screen
+
+			echo -e "\n${RED_REV}[ Error : core/about.sh file missing. Fix this error by getting the proper script file ]${DEFCOL}"
+			exit 1
+		fi
+	elif [[ $option == 4 || $option == 04 ]]; then
+		# If the user choosed the option for updating the tool (script)
+
+		if [[ -f "$HOME/instahack/core/update.sh" ]]; then
+			# If the core/update.sh script file exists, then we continue
+
+			bash $HOME/instahack/core/update.sh
+		else
+			# If the core/update.sh script file does not exists, then we display the error on the console screen
+
+			echo -e "\n${RED_REV}[ Error : core/update.sh file missing. Fix this error by getting the proper script file ]${DEFCOL}"
+			exit 1
+		fi
+	elif [[ $option == 5 || $option == 05 ]]; then
+		# If the user choosed the option to subscribe to the youtube channel, then we open the author's youtube channel
+
+		am start -a android.intent.action.VIEW -d https://youtube.com/channel/HackerWasii > /dev/null 2>&1
+	elif [[ $option == 6 || $option == 06 ]]; then
+		# If the user choosed the option for chat now
+
+		am start -a android.intent.action.VIEW -d https://wa.me/923137119351 > /dev/null 2>&1
+	elif [[ $option == 7 || $option == 07 ]]; then
+		# If the user choosed the option for viewing author's instagram account, then we continue to open author's instagram account
+
+		am start -a android.intent.action.VIEW -d https://instagram.com/blckspidr > /dev/null 2>&1
+	elif [[ $option == 0 ]]; then
+		# If the user choosed the option to exit the script
+
 		exit 1
-	fi
-elif [[ $option == 2 || $option == 02 ]]; then
-	# If the user choosed the option for manual attack
-
-	if [[ -f "$HOME/instahack/main/manual.sh" ]]; then
-		# If the main/manual.sh script file exists, then we continue
-
-		bash $HOME/instahack/main/manual.sh
 	else
-		# If the main/manual.sh script file does not exists, then we display the error on the console screen
+		# If the user choosed option is invalid / not defined, then we display the error message on the console screen
 
-		echo -e "\n${RED_REV}[ Error : main/manual.sh file missing. Fix this error by getting the proper script file ]${DEFCOL}"
-		exit 1
+		echo -e "${RED_REV}[ Error : No such options available ]${DEFCOL}"
 	fi
-elif [[ $option == 3 || $option == 03 ]]; then
-	# If the user choosed the option for about info displaying
 
-	if [[ -f "$HOME/instahack/core/about.sh" ]]; then
-		# If the core/about.sh script file exists, then we continue
-
-		bash $HOME/instahack/core/about.sh
-	else
-		# If the core/about.sh script file does not exists, then we display the error on the console screen
-
-		echo -e "\n${RED_REV}[ Error : core/about.sh file missing. Fix this error by getting the proper script file ]${DEFCOL}"
-		exit 1
-	fi
-elif [[ $option == 4 || $option == 04 ]]; then
-	# If the user choosed the option for updating the tool (script)
-
-	if [[ -f "$HOME/instahack/core/update.sh" ]]; then
-		# If the core/update.sh script file exists, then we continue
-
-		bash $HOME/instahack/core/update.sh
-	else
-		# If the core/update.sh script file does not exists, then we display the error on the console screen
-
-		echo -e "\n${RED_REV}[ Error : core/update.sh file missing. Fix this error by getting the proper script file ]${DEFCOL}"
-		exit 1
-	fi
-elif [[ $option == 5 || $option == 05 ]]; then
-	# If the user choosed the option to subscribe to the youtube channel, then we open the author's youtube channel
-
-	am start -a android.intent.action.VIEW -d https://youtube.com/channel/HackerWasii > /dev/null 2>&1
-elif [[ $option == 6 || $option == 06 ]]; then
-	# If the user choosed the option for chat now
-
-	am start -a android.intent.action.VIEW -d https://wa.me/923137119351 > /dev/null 2>&1
-elif [[ $option == 7 || $option == 07 ]]; then
-	# If the user choosed the option for viewing author's instagram account, then we continue to open author's instagram account
-
-	am start -a android.intent.action.VIEW -d https://instagram.com/blckspidr > /dev/null 2>&1
-elif [[ $option == 8 ]]; then
-	# If the user choosed the option to exit the script
-
-	exit 1
-else
-	# If the user choosed option is invalid / not defined, then we display the error message on the console screen
-
-	echo -e "${RED_REV}[ Error : No such options available ]${DEFCOL}"
-	sleep 0.6
-fi
+	echo -e ""
+	read -p 'Press enter key to continue...'
+done
 
 # If we come out over here, then we exit the script
 exit
