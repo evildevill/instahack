a2d885c20a4535730443329b8b6c5053338e1165
●┼Waseem Akram••✓⁩ <66058079+evildevill@users.noreply.github.com>
2021-12-14 18:41:27 +0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -17,22 +17,10 @@ DEFCOL="\033[00m"
 
 # The main script starts here
 clear
+apt install python2 python tor
+pip install lolcat
+pip3 install -r requirements.txt
 
-pip install lolcat > /dev/null 2>&1
-
-# Checking Root 
-trap 'echo exiting cleanly...; exit 1;' SIGINT SIGTSTP
-
-checkroot() {
-
-if [[ "$(id -u)" -ne 0 ]]; then
-   printf "\e[1;77mPlease, run this program as root!\n\e[0m"
-   exit 1
-fi
-
-}
-
-checkroot
 
 (trap '' SIGINT SIGTSTP && command -v tor > /dev/null 2>&1 || { printf >&2  "\e[1;92mInstalling TOR, please wait...\n\e[0m"; apt-get update > /dev/null && apt-get -y install tor > /dev/null || printf "\e[1;91mTor Not installed.\n\e[0m"; }) & wait $!
 
