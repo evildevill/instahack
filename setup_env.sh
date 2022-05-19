214877e8d94c0b4f9d22e8b398414614ccc66a3a
●┼Waseem Akram••✓⁩ <66058079+evildevill@users.noreply.github.com>
2022-05-19 09:30:06 +0500

    Create setup_env.sh

---
+++ setup_env.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+# This script automates the creation of Python virtual environment.
+
+if [ -d "venv" ]; then
+    echo "Virtual environment 'virtualenv' found, activating it."
+else
+    echo "Virtual environment not found, creating new 'virtual env'."
+    python3 -m venv venv
+    if [ $? -eq 0 ]; then
+        echo "Virtual environment was successfully created."
+    else
+        echo "Virtual environment was NOT created, aborting."
+        exit 1
+    fi
+fi
+
+source venv/bin/activate
+if [ $? -eq 0 ]; then
+    echo "Virtual environment is successfully activated."
+else
+    echo "Virtual environment was NOT activated, aborting."
+    exit 1
+fi
+
+echo "Installing required packages."
+sleep 2.0
+pip3 install -r requirements.txt
+if [ $? -eq 0 ]; then
+    sleep 2.0
+    echo "All requirements were successfully installed."
+    bash setup.sh
+else
+    echo "Requirements were NOT installed properly, aborting."
+    exit 1
+fi
+
+echo "Done."
