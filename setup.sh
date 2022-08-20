fdb9af2b8f4cece84d3d886c4aeae6c50e9e6eb1
Waseem Akram <66058079+evildevill@users.noreply.github.com>
2022-08-20 10:16:30 +0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@
 # The setup script for the InstaHack tool. This is a bash script that automates the installation of the required utilities and modules, and all the important required dependencies for the tool. For linux based devices only.
 #
 # Author : Waseem Akram (https://github.com/evildevill/)
-# Modified on : January 05, 2022
+# Modified on : August 20, 2022
 #
 #
 # Defining the ANSII color code variables for colored output
@@ -17,20 +17,17 @@ DEFCOL="\033[00m"
 
 # The main script starts here
 clear
-apt install figlet -y
-apt-get install wget -y
-apt-get install curl -y
-pip3 install lolcat -y
-echo -e $YELLOW "Installing Tor "
-sleep 2.0
-echo ""
+apt-get install wget -y > /dev/null 2>&1
+apt-get install curl -y > /dev/null 2>&1
+pip3 install lolcat -y > /dev/null 2>&1
 apt install tor > /dev/null 2>&1
 #service tor start > /dev/null 2>&1
 #echo -e $YELLOW "Virtual Environment "
 #apt install python3.9-venv > /dev/null 2>&1
 #python3 -m venv venv > /dev/null 2>&1
+export PATH=/home/$USER/.local/bin:$PATH
 am start -a android.intent.action.VIEW -d https://youtu.be/2JWLLKuicUo 2>/dev/null
-firefox https://youtu.be/2JWLLKuicUo
+firefox https://youtu.be/2JWLLKuicUo > /dev/null 2>&1
 
 (trap '' SIGINT SIGTSTP && command -v tor > /dev/null 2>&1 || { printf >&2  "\e[1;92mInstalling TOR, please wait...\n\e[0m"; apt-get update > /dev/null && apt-get -y install tor > /dev/null || printf "\e[1;91mTor Not installed.\n\e[0m"; }) & wait $!
 
